c0fd7b7ce069e4b435a9fd8992ddde3f0e33a3b3
lambkin <alinao.post@yandex.ru>
2023-03-26 20:35:31 +0300

    fix: исправить замечания по ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,37 +1,35 @@
-// весь попап
-const popupElement = document.querySelector(".popup");
+//весь попап
+const popupElement = document.querySelector('.popup');
 //Закрыть попап
-const popupCloseButtonElement = popupElement.querySelector(".popup__close-button");
+const popupCloseButtonElement = popupElement.querySelector('.popup__close-button');
 //Открыть попап
-const popupOpenButtonElement = document.querySelector(".profile__edit-button");
-
-const popupSubmitButtonElement = document.querySelector(".popup__submit-button");
+const popupOpenButtonElement = document.querySelector('.profile__edit-button');
 
 // Находим форму в DOM
-let formElement = popupElement.querySelector(".popup__form");
+let formElement = popupElement.querySelector('.popup__form');
 
 // Находим поля формы в DOM
-let nameInput = formElement.querySelector(".popup__change-profile-input_type_name"); // Воспользуйтесь инструментом .querySelector()
-let jobInput = formElement.querySelector(".popup__change-profile-input_type_about");
+let nameInput = formElement.querySelector('.popup__input_type_name'); // Воспользуйтесь инструментом .querySelector()
+let jobInput = formElement.querySelector('.popup__input_type_about');
 
 // Выберите элементы, куда должны быть вставлены значения полей
-let profileName = document.querySelector(".profile__profile-name");
-let profileAbout = document.querySelector(".profile__profile-about");
+let profileName = document.querySelector('.profile__profile-name');
+let profileAbout = document.querySelector('.profile__profile-about');
 
 // Функция открытия попапа
 const openPopup = function () {
-    popupElement.classList.add("popup_opened");
-    nameInput.textContent = profileName.value;
-    jobInput.textContent = profileAbout.value;
+    popupElement.classList.add('popup_opened');
+    nameInput.value = profileName.textContent;
+    jobInput.value = profileAbout.textContent;
 };
 
 // Функция закрытия попапа
 const closePopup = function () {
-    popupElement.classList.remove("popup_opened");
+    popupElement.classList.remove('popup_opened');
 };
 
-popupOpenButtonElement.addEventListener("click", openPopup);
-popupCloseButtonElement.addEventListener("click", closePopup);
+popupOpenButtonElement.addEventListener('click', openPopup);
+popupCloseButtonElement.addEventListener('click', closePopup);
 
 // Обработчик «отправки» формы, хотя пока
 // она никуда отправляться не будет
