17791349832d8628a208c52b164f8e3a291fc187
lambkin <alinao.post@yandex.ru>
2023-04-15 19:20:24 +0300

    fix: исправить замечания по ревью

--- scripts/index.js
+++ scripts/index.js
@@ -95,34 +95,6 @@ formChangeProfileElement.addEventListener('submit', handleFormSubmit);
 
 // ШЕСТЬ КАРТОЧЕК "ИЗ КОРОБКИ"
 
-// Массив
-const initialCards = [
-  {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-  },
-  {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  }
-];
-
 // Функция создания элемента
 function createElement (item) {
   const newElement = elementTemplate.cloneNode(true);
@@ -166,6 +138,8 @@ initialCards.forEach((element) => {
 
 // Открыть попап добавления карточки
 popupAddElementOpenButton.addEventListener('click', function () {
+  formAddElementName.value = 'Название';
+  formAddElementImg.value = 'Ссылка на картинку';
   openPopup(popupAddElement);
 });
 
